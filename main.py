2cdd4da2fe793a42f5be340eab2d7b9afe094090
Riley Williams <ririwilliams.rw@gmail.com>
2024-11-01 00:26:33 -0500

    Added player class and drew it to the screen successfully

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
         for event in pygame.event.get():
@@ -13,9 +15,12 @@ def main():
                 return
                 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        
+        
 
         dt = game_clock.tick(60)/1000
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
