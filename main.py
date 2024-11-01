6ec4ab02f0b934c3fd23781bc06259ee8aa4b248
Riley Williams <ririwilliams.rw@gmail.com>
2024-10-31 23:59:26 -0500

    Added circle class for the sprites

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while(True):
         for event in pygame.event.get():
@@ -13,4 +15,7 @@ def main():
         screen.fill((0,0,0))
         pygame.display.flip()
 
-main()
+        dt = game_clock.tick(60)/1000
+
+if __name__ == "main":
+    main()
