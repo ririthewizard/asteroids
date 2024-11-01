5ed7820b2295b177551be737e276d2dc8d124100
Riley Williams <ririwilliams.rw@gmail.com>
2024-11-01 01:15:46 -0500

    Changed all Player instances to Groups

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    dt = 0
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
                 
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
         
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
         
 
         dt = game_clock.tick(60)/1000
--- player.py
+++ player.py
@@ -29,3 +29,13 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]: # right
             self.rotate(dt)
+
+        if keys[pygame.K_w]: # up
+            self.move(dt)
+        
+        if keys[pygame.K_s]: # down
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
