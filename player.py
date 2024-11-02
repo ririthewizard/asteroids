fde707ff19fdde4fc2ae4600791dad8d1747b693
Riley Williams <ririwilliams.rw@gmail.com>
2024-11-02 13:26:29 -0500

    Added Shot class and implemented a shot cooldown so it is not a constant stream of bullets

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,6 +24,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]: # left
@@ -36,6 +39,17 @@ class Player(CircleShape):
         if keys[pygame.K_s]: # down
             self.move(-dt)
 
+        if keys[pygame.K_SPACE]: #spacebar
+            self.shoot()
+
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
